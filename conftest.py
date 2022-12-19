08d16c979ed3e7806f7a2dd52d02d75213d9f94c
wowmagik <115688004+wowmagik@users.noreply.github.com>
2022-12-19 16:31:26 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,16 +1,36 @@
-from selenium import webdriver
 import pytest
-
+from settings import valid_email, valid_password
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 @pytest.fixture(autouse=True)
 def testing():
-   pytest.driver = webdriver.Chrome('C:\\WebDriver\\bin\\chromedriver.exe')
+   pytest.driver = webdriver.Chrome('/tests_drivers/chromedriver.exe')
 
-   pytest.driver.set_window_size(1400, 1000)
    # Переходим на страницу авторизации
-   pytest.driver.get('http://petfriends1.herokuapp.com/login')
+   pytest.driver.get('https://petfriends.skillfactory.ru/login')
 
    yield
 
-
    pytest.driver.quit()
+
+@pytest.fixture()
+def go_to_my_pets():
+
+   element = WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.ID, "email")))
+   # Вводим email
+   pytest.driver.find_element_by_id('email').send_keys(valid_email)
+
+   element = WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.ID, "pass")))
+   # Вводим пароль
+   pytest.driver.find_element_by_id('pass').send_keys(valid_password)
+
+   element = WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[type='submit']")))
+   # Нажимаем на кнопку входа в аккаунт
+   pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
+
+   element = WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Мои питомцы")))
+   # Нажимаем на ссылку "Мои питомцы"
+   pytest.driver.find_element_by_link_text("Мои питомцы").click()
